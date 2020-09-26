f95ece2009255ad30c4861c9ea7fca97400d7dec
“Alejandro1709” <alejandro.lr.developer@hotmail.com>
2020-09-26 17:52:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let matrix = [
+  [1, 0, 1, 0],
+  [0, 1, 1, 0],
+  [1, 0, 1, 0],
+  [1, 0, 1, 0],
+];
+
+for (var i = 0; i < matrix.length; i++) {
+  var doc = document.createElement('div');
+  doc.className = 'item-holder';
+  //   console.log(doc);
+
+  for (var j = 0; j < matrix.length; j++) {
+    doc.innerHTML += `<div class="item">${matrix[i][j]}</div>`;
+    // console.log(`j = ${j}`);
+    // console.log(`[${i}][${j}]`);
+    document.getElementsByClassName('container')[0].appendChild(doc);
+  }
+}
